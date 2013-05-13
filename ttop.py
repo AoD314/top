7c06feb72c015df8808bdf719469fe369d7fd36b
AoD314 <morozov.andrey.vmk@gmail.com>
2013-05-12 21:26:46 -0300

    a lot of refactoring in the ttop.

--- ttop.py
+++ ttop.py
@@ -5,40 +5,70 @@ import sys
 import subprocess
 import time
 
-def main():
-    cmd = "cat /proc/cpuinfo | grep 'model name' | wc -l"
-    cpu_cout = subprocess.getoutput(cmd)
 
-    cmd = "cat /proc/cpuinfo | grep 'model name' | uniq"
-    cpu_name = subprocess.getoutput(cmd)    
-    cpu_name = cpu_name[cpu_name.find(":") + 1:]
-    for i in range(len(cpu_name)):
-        cpu_name = cpu_name.replace("  ", " ")
+class CPU:
+    def __init__(self):
+        self.count = int(subprocess.getoutput("cat /proc/cpuinfo | grep 'model name' | wc -l"))
 
-    cmd = "nvidia-smi -a | grep 'Product Name'"
-    gpu_name = str(subprocess.getoutput(cmd).replace("\n", " "))
-    gpu_name = gpu_name[gpu_name.find(':')+1:]
+        cpu_name = subprocess.getoutput("cat /proc/cpuinfo | grep 'model name' | uniq")
+        self.name = cpu_name[cpu_name.find(":") + 1:].replace('  ', ' ').replace('  ', ' ').replace('  ', ' ')
 
-    sys.stdout.write("[" + cpu_cout.rjust(len(cpu_cout) + 1) + " x" + cpu_name.rjust(len(cpu_name)) + " ]" + "   [" + gpu_name.rjust(len(gpu_name)) + " ]\n")
+    def name(self):
+        return str(self.name)
 
-    while True:
+    def count(self):
+        return self.count
+
+    def temperature(self):
         cmd = "sensors | grep 'CPU Temperature' | awk '{print $3}'"
         temp_cpu = subprocess.getoutput(cmd)
-        if (temp_cpu == ""):
-            cmd = "sensors | grep 'Core 0' | awk '{print $3}'"
-            temp_cpu = subprocess.getoutput(cmd)
+        i = self.count - 1
+        while temp_cpu == "":
+            temp_cpu = subprocess.getoutput("sensors | grep 'Core " + str(i) + "' | awk '{print $3}'")
+            i -= 1
+            if i < 0:
+                break
+        return float(temp_cpu[:-2])
 
-        degr = temp_cpu[-2:]
 
-        cmd = "nvidia-smi -q -d TEMPERATURE -i 0 | grep 'Gpu' | awk '{print $3}'"
-        temp_gpu = subprocess.getoutput(cmd)
-        temp_gpu = "+" + temp_gpu + ".0" + degr
+class GPU:
+    def __init__(self):
+        gpu_name = str(subprocess.getoutput("nvidia-smi -a | grep 'Product Name'").replace("\n", " "))
+        self.name = gpu_name[gpu_name.find(':') + 1:]
 
-        mess = temp_cpu + " " + temp_gpu
-        sys.stdout.write("\r" + repr(temp_cpu).rjust(len(cpu_name) + 6 + len(cpu_cout)) + repr(temp_gpu).rjust(len(gpu_name) + 6))
-        sys.stdout.flush()
+    def name(self):
+        return str(self.name)
+
+    def temperature(self):
+        return float(subprocess.getoutput("nvidia-smi -q -d TEMPERATURE -i 0 | grep 'Gpu' | awk '{print $3}'") + ".0")
+
+
+class Printer:
+    def __init__(self, cpu_count, cpu_name, gpu_name):
+        self.cpu_len = 6 + len(cpu_name) + len(str(cpu_count))
+        self.gpu_len = 6 + len(gpu_name)
+        sys.stdout.write("[" + str(cpu_count).rjust(len(str(cpu_count)) + 1) + " x"
+                         + cpu_name.rjust(len(cpu_name)) + " ]"
+                         + "   [" + gpu_name.rjust(len(gpu_name))
+                         + " ]\n")
+
+    def status(self, cpu_temp, gpu_temp):
+        cpu = str(cpu_temp) + '°C'
+        gpu = str(gpu_temp) + '°C'
+        sys.stdout.write("\r" + repr(cpu).rjust(self.cpu_len) + repr(gpu).rjust(self.gpu_len))
+
+
+def main():
+    cpu = CPU()
+    gpu = GPU()
+    printer = Printer(cpu.count, cpu.name, gpu.name)
+
+    while True:
+        printer.status(cpu.temperature(), gpu.temperature())
         time.sleep(1)
+        if input() == 'q':
+            break
+
 
 if __name__ == '__main__':
     main()
-
