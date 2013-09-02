e751613a702dd2898ec01ba96a966ee71926a58b
AoD314 <morozov.andrey.vmk@gmail.com>
2013-09-01 23:59:29 -0300

    added new application that show the short list of big files.

---
+++ bigs.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import os
+
+def size_to_str(size):
+    # B-> K -> M -> G -> T
+    s = [" B", " K", " M", " G", " T", " P", " E", " Z", " Y"]
+
+    for i in range(len(s)):
+        if size <= 9999:
+            j = len(str(int(size)))
+            f = ''
+            if (2-j >= 0) and i != 0:
+                f = "{" + ":.{0}f".format(3-j) + "}"
+            else:
+                f = "{0:4}"
+                size = int(size)
+            size_s = f.format(size) + s[i]
+            return size_s
+        size /= 1000.0
+
+def main(path):
+    dic = []
+    files = os.listdir(path)
+    total_s = 0
+    for f in files:
+        full_name = os.path.join(path, f)
+        sz = int(os.path.getsize(full_name))
+        total_s += sz
+        dic += [{'name': f, 'size': sz}]
+    dic.sort(key=lambda x: -x['size'])
+
+    print(path + ' : ' + size_to_str(total_s))
+    for i in dic[:50]:
+        print("{0}    {1}".format(size_to_str(i['size']), i['name']))
+
+if __name__ == '__main__':
+    main(os.getcwd())
